b0c3409c324a7a5a8253b3538737119c262557b8
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-07-07 20:31:22 +0530

    index.tsx Refactored

--- app/index.tsx
+++ app/index.tsx
@@ -2,13 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View>
       <Text>LinkedIn Clone | React Native | Mobile Application</Text>
     </View>
   );
