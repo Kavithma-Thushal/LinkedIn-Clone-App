97adbc9f41e6a32c289c599e9c1ba48c45a5d5fc
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-07-18 11:55:22 +0530

    Job Screen Added

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { View, StyleSheet } from 'react-native';
-import MainFeed from '@/components/MainFeed';
-import ProfileHeader from '@/components/ProfileHeader';
+import MainFeed from '../components/MainFeed';
+import ProfileHeader from '../components/ProfileHeader';
 
 export default function Index() {
 
